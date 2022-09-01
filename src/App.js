16fffbf9cdac3ba528c2966eb342dde814036b6f
Benedikt Kaltenecker <benedikt@kaltenecker.org>
2022-09-01 15:51:30 +0200

    provided navbar dummy

--- src/App.js
+++ src/App.js
@@ -2,29 +2,31 @@ import './App.css';
 import Axios from "axios";
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import Navbar from './Components/Navbar'
 
 function App() {
 
-  const [recipes, setRecipes] = useState([""])
+  const [recipes, setRecipes] = useState([])
 
   useEffect(() => {
     axios.get("https://preview.contentful.com/spaces/d6gach6xgkb5/environments/master/entries?access_token=-a1pu0dqYGnV2VkL1juU49xpUz_Ht49i30ndE_J94gc")
-      .then((response) => { setRecipes(response.data) })
+      .then((response) => { setRecipes(response.data.items) })
       .catch((err) => { console.log(err) })
   }, []);
 
 
   return (
     <div className="App">
-      {/* {recipes.map((recipe, index) => {
+      <Navbar />
+      {recipes.map((recipe, index) => {
         console.log(recipe);
-        // return (
-        //   <div key={recipe.sys.id}>
-        //     <h2>{recipe.items.fields.header}</h2>
-
-        //   </div>
-        // );
-      })} */}
+        return (
+          <div key={recipe.sys.id}>
+            <h2>{recipe.fields.header}</h2>
+            <img src={`https:${recipe.fields.picture}`} />
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return <nav className="nav">
+        <img src='https://img.freepik.com/premium-vector/cookbook-logo-icon-symbol-with-recipe-book-with-cook-simple-negative-space-style-logo_279597-883.jpg' classname="site-title" />
+        <div>
+            <input className="search" type="text" placeholder="Find yummy stuff!"/>
+            <button className="button" type="submit" value="submit">Search</button>
+        </div>
+        <ul>
+            <li href="/recipes">Recipes</li>
+            <li href="/submit">Submit Recipe</li>
+        </ul>
+    </nav>
+}
