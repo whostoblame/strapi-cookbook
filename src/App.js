b9e2cfeac2438075194d1eb8e3848801aaf9958d
Cristina Caran <cristinacaran@Cristinas-Air.fritz.box>
2022-09-08 22:56:30 +0200

    Cleaned App.js, made Router work and created Recipes.js for the axios request

--- src/App.js
+++ src/App.js
@@ -1,55 +1,30 @@
 import './App.css';
-import { useEffect, useState } from "react";
-import axios from 'axios';
 import Navbar from './Components/Navbar';
 import { useNavigate } from "react-router-dom";
-import Recipe from './Components/Recipe';
-import GetPictures from "./Components/GetPictures"
+import Recipes from "./Components/Recipes";
+import Home from "./Components/Home";
+import {Routes, Route } from "react-router-dom";
+
 
 function App() {
 
   const navigate = useNavigate();
-  const [recipes, setRecipes] = useState([])
-
-  useEffect(() => {
-    axios.get("https://cdn.contentful.com/spaces/d6gach6xgkb5/environments/master/entries?access_token=ArEcNdHZeR9OpdsONr1H-_4VoboSlQjXiBeuYfu5RPo")
-      .then((response) => {
-        setRecipes(response.data.items);
-        //console.log(response.data.assets) 
-      })
-      .catch((err) => { console.log(err); })
-  }, []);
-
-  const changePage = (id) => {
-    navigate(`/recipes/${id}`)
-  }
-
-  /* save assets in a state. save img in array. */
+ 
   return (
-    <div className="App">
-
-
-      <Navbar />
-      {recipes.map((recipe, index) => {
-        console.log(recipe)
-        return (
-          <div key={recipe.sys.id} >
-            <h2>{recipe.fields.header}</h2>
-            <GetPictures id={recipe.fields.picture[0].sys.id} />
-            <button onClick={() => { changePage(recipe.sys.id) }}>View more</button>
-            {recipe.fields.receiptText.content.map((content, i) => {
-
-              if (content.nodeType === "paragraph") {
-                return (<p key={recipe.sys.id + i}> {content.content[0].value} </p>);
-              }
-
-            })}
-
-          </div>
-        );
-      })}
-    </div>
+  
+      <Routes> 
+      <Route path='/' element={<Home />}/>
+      <Route path='/recipe' element={<Recipes />}/>
+      </Routes>
+    
   );
 }
 
 export default App;
+
+
+
+/*<Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/recipes/:id" element={<Recipe />} />
+      </Routes> */
--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Recipe from './Components/Recipe';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/recipes/:id" element={<Recipe />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
