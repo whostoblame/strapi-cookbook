c9788ba73410d2e040296aab0b1ccaa8bf85888a
Benedikt Kaltenecker <benedikt@kaltenecker.org>
2022-09-09 15:11:55 +0200

    minor styling changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+    const navigate = useNavigate();
+
     return <nav className="nav">
-        <img src='https://img.freepik.com/premium-vector/cookbook-logo-icon-symbol-with-recipe-book-with-cook-simple-negative-space-style-logo_279597-883.jpg' className="site-title" />
+        <img onClick={() => { navigate(`/`) }} src='https://img.freepik.com/premium-vector/cookbook-logo-icon-symbol-with-recipe-book-with-cook-simple-negative-space-style-logo_279597-883.jpg' className="site-title" />
         <div>
             <input className="search" type="text" placeholder="Find yummy stuff!" />
-            <button className="button" type="submit" value="submit">Search</button>
+            <Link to="/recipes/6UYdAs3y5oxB5cPs0wyMQ3"><button className="button" type="submit" value="submit">Search</button></Link>
         </div>
         <div>
             <Link className="navLinks" to="/">Recipes</Link>
